ef65b876692a33539fa9a82662eac619b7752e1a
Kearsz <16836684+Kearsz@users.noreply.github.com>
2021-10-12 13:55:08 +0300

    mains done
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- Readme.md
+++ Readme.md
@@ -1,1 +1,19 @@
-Readme fiel
+Application requirements:
+
+    The items entered in the field are assembled into a list on the page by pressing the button. done
+    The content of the input fields must be checked for blank as well as incorrect (eg too short) content done
+    Incorrect content in the input field causes an error message as well as an incorrect field highlighting (eg red border). done
+    It must be possible to remove elements from the list and mark the task as done 
+    The application CSS and JavaScript must be specified in an external CSS file done
+    The information in the list is stored in a browser, eg localstorage  done
+
+
+Additional functions:
+    You can implement a counter that shows the number of open items in the list. Done
+    You may also use drop-down menus and check boxes  not done
+    Try to come up with some meaningful way to hide and show elements in the app.  not done skip
+    If you wish, you can also use the drag & drop functionality of HTML5 in the list not not done
+    You can implement buttons that only display active or completed tasks  not done
+
+
+
---
+++ SCRIPT.js
@@ -0,0 +1,170 @@
+//selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
+
+//eventti Kuuntelijat
+//lataa kaikki tiedot sivun latautuessa
+document.addEventListener("DOMContentLoaded", getTodos);
+
+todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
+//functions
+function addTodo(e){
+
+  // estää listan lähettämästä
+  e.preventDefault();
+
+  // tekee todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //Tekee li rivin
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+
+  if(todoInput.value == null || todoInput.value.length < "3"){
+    alert("You must write something!");
+    document.getElementById("input").style.borderColor = "red";
+  }else{
+      document.getElementById("input").style.borderColor = "white";
+      // Lisätään todo Localstorage
+      saveLocalTodos(todoInput.value);
+
+      newTodo.classList.add("todo-item");
+      todoDiv.appendChild(newTodo);
+      
+      // tyhjennä todo arvo input kentältä
+      todoInput.value = ""; 
+
+      // valmis nappi
+      const completedButton = document.createElement("button");
+      completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+      completedButton.classList.add("valmis-btn");
+      todoDiv.appendChild(completedButton);
+
+      // poista nappi
+      const removeButton = document.createElement("button");
+      removeButton.innerHTML = `<i class="fas fa-trash"></i>`;
+      removeButton.classList.add("poista-btn");
+      todoDiv.appendChild(removeButton);
+
+      // lisää listan
+      todoList.appendChild(todoDiv);
+  }
+}
+// poisto ja valmis checkaus
+function deleteCheck(e){
+  const item = e.target;
+//poistaa rivin
+  if(item.classList[0] === "poista-btn"){
+      const todo = item.parentElement;
+      removeLocalTodos(todo);
+      todo.remove();
+    }
+    // valmis chekkaus
+  if(item.classList[0] === "valmis-btn"){
+      const todo = item.parentElement;
+      todo.classList.toggle('completed');
+    }
+}
+//filteri
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      // näyttää kaikki tehtävät
+      case "all":
+        todo.style.display = "flex";
+        break;
+      // ainoastaa näyttää valmiiks tehdyt
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      // näyttää vain tekemättä olevat tehtävät
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  });
+}
+
+//lokaalinen varasto
+function saveLocalTodos(todo){
+  // tarakistaa jos todo lista on jo olemassa, jos ei ole niin se tekee uuden tyhjän
+  if(localStorage.getItem("todos")=== null){
+    todos = [];
+  // jos meillä on jo todo lista ja hakee todo listan lokaaliselta storagesta.  
+  }else{
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  // tallentaa uuden tuloksen storageen
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+// poistaa ja tallentaa lokaaliselta storagesta tiedon
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  // kysyy mistä aloitetaan poistaminen
+  const todoIndex = todo.children[0].innerText;
+
+  // kysyy kuinka monta riviä poistaan
+  todos.splice(todos.indexOf(todoIndex), 1);
+
+  // tallanetaa loppu tuloksen storageen
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function(todo) {
+  // tekee todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo")
+
+  //Tekee li rivin
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todo;
+
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // valmis nappi
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("valmis-btn");
+  todoDiv.appendChild(completedButton);
+
+  // poista nappi
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  removeButton.classList.add("poista-btn");
+  todoDiv.appendChild(removeButton);
+
+  // lisää listan
+  todoList.appendChild(todoDiv);
+
+  });
+}
+
