e3a05142f672e3541cc6a86d7f994876b445afb9
otikp <91963309+otikp@users.noreply.github.com>
2021-10-21 12:38:56 +0300

    Update SCRIPT.js
    Changes to comments to make them more clear

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,24 +1,24 @@
-//selectors 
+//Selectors 
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector(".filter-todo");
 
-//eventti Kuuntelijat
-//lataa kaikki tiedot sivun latautuessa
+//Event kuuntelijat
+//Lataa kaikki tiedot sivun latautuessa
 document.addEventListener("DOMContentLoaded", getTodos);
 
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener("click", filterTodo);
 
-//functions
+//Functiolla lisätään todot
 function addTodo(e){
 
-  // estää listan lähettämästä
+  // Estää listan lähettämästä ja sivun päivyytämästä
   e.preventDefault();
 
-  // tekee todo div
+  //Tehdään tododiv
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
 
@@ -37,50 +37,50 @@ function addTodo(e){
       newTodo.classList.add("todo-item");
       todoDiv.appendChild(newTodo);
       
-      // tyhjennä todo arvo input kentältä
+      // Tyhjennä todo arvo input kentältä
       todoInput.value = ""; 
 
-      // valmis nappi
+      // Valmis nappula
       const completedButton = document.createElement("button");
       completedButton.innerHTML = `<i class="fas fa-check"></i>`;
       completedButton.classList.add("valmis-btn");
       todoDiv.appendChild(completedButton);
 
-      // poista nappi
+      // Poista nappi
       const removeButton = document.createElement("button");
       removeButton.innerHTML = `<i class="fas fa-trash"></i>`;
       removeButton.classList.add("poista-btn");
       todoDiv.appendChild(removeButton);
 
-      // lisää listan
+      // Lisää listan
       todoList.appendChild(todoDiv);
   }
 }
-// poisto ja valmis checkaus
+// Poisto ja valmis checkaus
 function deleteCheck(e){
   const item = e.target;
-//poistaa rivin
+// Poistaa rivin
   if(item.classList[0] === "poista-btn"){
       const todo = item.parentElement;
       removeLocalTodos(todo);
       todo.remove();
     }
-    // valmis chekkaus
+    // Valmis chekkaus
   if(item.classList[0] === "valmis-btn"){
       const todo = item.parentElement;
       todo.classList.toggle('completed');
     }
 }
-//filteri
+// Todo filtteri
 function filterTodo(e) {
   const todos = todoList.childNodes;
   todos.forEach(function(todo) {
     switch (e.target.value) {
-      // näyttää kaikki tehtävät
+      // Näyttää kaikki tehtävät
       case "all":
         todo.style.display = "flex";
         break;
-      // ainoastaa näyttää valmiiks tehdyt
+      // Ainoastaa näyttää valmiiks tehdyt
       case "completed":
         if (todo.classList.contains("completed")) {
           todo.style.display = "flex";
@@ -88,7 +88,7 @@ function filterTodo(e) {
           todo.style.display = "none";
         }
         break;
-      // näyttää vain tekemättä olevat tehtävät
+      // Näyttää vain tekemättä olevat tehtävät
       case "uncompleted":
         if (!todo.classList.contains("completed")) {
           todo.style.display = "flex";
@@ -99,20 +99,20 @@ function filterTodo(e) {
   });
 }
 
-//lokaalinen varasto
+// Lokaalinen varasto
 function saveLocalTodos(todo){
-  // tarakistaa jos todo lista on jo olemassa, jos ei ole niin se tekee uuden tyhjän
+  // Tarakistaa jos todo lista on jo olemassa, jos ei ole niin se tekee uuden tyhjän
   if(localStorage.getItem("todos")=== null){
     todos = [];
-  // jos meillä on jo todo lista ja hakee todo listan lokaaliselta storagesta.  
+  // Jos meillä on jo todo lista ja hakee todo listan lokaaliselta storagesta.  
   }else{
     todos = JSON.parse(localStorage.getItem("todos"));
   }
-  // tallentaa uuden tuloksen storageen
+  // Tallentaa uuden tuloksen storageen
   todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
 }
-// poistaa ja tallentaa lokaaliselta storagesta tiedon
+// Poistaa ja tallentaa lokaaliselta storagesta tiedon
 function removeLocalTodos(todo) {
   let todos;
   if (localStorage.getItem("todos") === null) {
@@ -120,13 +120,13 @@ function removeLocalTodos(todo) {
   } else {
     todos = JSON.parse(localStorage.getItem("todos"));
   }
-  // kysyy mistä aloitetaan poistaminen
+  // Kysyy mistä aloitetaan poistaminen
   const todoIndex = todo.children[0].innerText;
 
-  // kysyy kuinka monta riviä poistaa
+  // Kysyy kuinka monta riviä poistaa
   todos.splice(todos.indexOf(todoIndex), 1);
 
-  // tallanetaa loppu tuloksen storageen
+  // Tallanetaa loppu tuloksen storageen
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
@@ -139,7 +139,7 @@ function getTodos() {
     todos = JSON.parse(localStorage.getItem("todos"));
   }
   todos.forEach(function(todo) {
-  // tekee todo div
+  // Tekee todo div
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo")
 
@@ -150,19 +150,19 @@ function getTodos() {
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
-  // valmis nappi
+  // Valmis nappi
   const completedButton = document.createElement("button");
   completedButton.innerHTML = `<i class="fas fa-check"></i>`; 
   completedButton.classList.add("valmis-btn");
   todoDiv.appendChild(completedButton);
 
-  // poista nappi
+  // Poista nappi
   const removeButton = document.createElement("button");
   removeButton.innerHTML = `<i class="fas fa-trash"></i>`;
   removeButton.classList.add("poista-btn");
   todoDiv.appendChild(removeButton);
 
-  // lisää listan
+  // Lista lisätään
   todoList.appendChild(todoDiv);
 
   });
